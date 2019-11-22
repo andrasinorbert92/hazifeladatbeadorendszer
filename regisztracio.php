a28df77a61e82b94e290d8b4c224d94bdbfbe5fc
fulopmartin21 <fulop.martin21@gmail.com>
2019-11-22 15:30:30 +0100

    Az elso fajlok

---
+++ regisztracio.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta charset="UTF-8" lang="HU">
+<link rel="stylesheet" type="text/css" href="../css/fooldal.css">
+<title>Feliratkozás hírlevelünkre</title>
+</head>
+<body>
+<div class="main" style="width:40%">
+<form action="elfogadas.php" method="POST">
+<img src="../kepek/hirlevel.jpg" alt="hirlevel_logo"><br>
+Vezetéknév:<input type="text" name="vezeteknev" size="50" required placeholder="pl.:Szabó"><br>
+Keresztnév:<input type="text" name="keresztnev" size="50" required placeholder="pl.:István"><br>
+Kor:<input type="text" name="kor" size="55" required placeholder="pl.:20"><br>
+Város:<input type="text" name="lakcim" size="60" required placeholder="pl.:Jászberény"><br>
+E-mail:<input type="text" name="email" size="60" required placeholder="pl.:valami@valami.hu"><br>
+<input type="submit" value="Feliratkozás">
+</form>
+</div>
+</body>
+</html>
---
+++ sqlkapcsolat.php
@@ -0,0 +1,11 @@
+<html lang="hu">
+	<head>
+		<meta charset="utf-8">
+	</head>
+<body>
+<?php
+$kapcsolat=mysqli_connect("localhost","root","","regisztracio");
+mysqli_set_charset($kapcsolat,"utf8mb4");
+?>
+</body>
+</html>
