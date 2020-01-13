0138c904551f9a099550e4a2a2fce569b206861c
Gabesz79 <pal.gabor1979@gmail.com>
2020-01-13 15:49:37 +0100

    Php felulet, html css-be tettem, a fajlokat atneveztem php-re es beirtam a legtobb helyre, hogy hol kell php-zni

---
+++ php/teacher/assignments.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <title>Kiírt házifeladatok</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/student.css">
+  </head>
+  <body>
+    <h1>Házifeladat-beadó Rendszer</h1>
+    <div id="valign">
+      <div id="box">
+        <h2 id="kiírt-házifeladatok">Kiírt házifeladatok</h2>
+
+        <ul id="hfs">
+          <?php
+           // Itt kell lekérés az adatbázisból.
+           // A kétféle formátum lent látható.
+           ?>
+          <li>
+            <form class="card">
+              <p class="subject">Történelem</p>
+              <p class="topic">A Dualizmus-kora - Házidolgozat</p>
+              <div class="flexer">
+                <p class="student">Diák neve: Kis Pista</p>
+                <a href="#"><p>Csatolmány letöltése</p></a>
+              </div>
+              <input type="number" min="1" max="5" placeholder="Osztályzat" />
+              <input type="submit" value="Osztályzat adása" />
+              <p class="date">2020.01.01</p>
+            </form>
+          </li>
+          <li>
+            <form class="card">
+              <p class="subject">Bevezetés az Informatikába</p>
+              <p class="topic">Egyszerű tömörítési algoritmusok</p>
+              <div class="flexer">
+                <p class="student">Diák neve: Kis Pista</p>
+                <p class="grade">Osztályzat: 3</p>
+              </div>
+              <p class="date">2019.06.12</p>
+            </form>
+          </li>
+        </ul>
+
+        <p class="centered"><a href="dashboard.php">Vissza</a></p>
+
+      </div>
+    </div>
+  </body>
+</html>
---
+++ php/teacher/create_assignment.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+    <title>Új házifeladat kiírása</title>
+    <link rel="stylesheet" href="/css/main.css">
+    
+    <link rel="stylesheet" href="/css/create_assignment.css">
+    
+  </head>
+  <body>
+    <h1>Házifeladat-beadó Rendszer</h1>
+    <div id="valign">
+      <div id="box">
+        <h2 id="új-házifeladat-kiírása">Új házifeladat kiírása</h2>
+
+        <form id="new_hf" action="create_assignment.php" method="post">
+          <select id="hf_class">
+            <?php
+             // Itt kell lekérni az osztályokat a szervertől
+             ?>
+            <option value="6b">6.b</option>
+            <option value="6c">6.c</option>
+            <option value="7a">7.a</option>
+          </select>
+          <input id="hf_subject" placeholder="Tantárgy..." />
+          <input id="hf_name" placeholder="Házifeladat neve..." />
+          <input id="hf_hatar" type="date" placeholder="Határidő..." />
+          <input type="submit" value="Házifeladat kiírása" />
+        </form>
+
+        <p class="centered"><a href="dashboard.php">Vissza</a></p>
+
+      </div>
+    </div>
+  </body>
+</html>
