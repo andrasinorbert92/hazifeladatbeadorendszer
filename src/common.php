9a00f73d90be07b499a65fe46491a7a24ef69bcb
Vaki <vakihun68@gmail.com>
2020-01-19 15:59:04 +0100

    bejelentkezés elkészítve

--- src/common.php
+++ src/common.php
@@ -1,29 +1,29 @@
 <?php
 function isLoggedIn() {
-if(isSet($_SESSION['loggedin']))
+if(isSet($_SESSION['belepve']))
 	return true;
 else
 	return false; 
 }
 
 function beleptet($name,$pswd,$conn) {
-	$sql = "SELECT id FROM users WHERE username='".$name."' AND passwd='".hash('sha256',$pswd)."'";
+	$sql = "SELECT id FROM users WHERE email='".$name."' AND passwd='".$pswd."'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
 	session_start();
     while($row = $result->fetch_assoc()) {
-        $_SESSION["loggedin"] = $row["id"];
+        $_SESSION["belepve"] = $row["id"];
 		$_SESSION["level"]= 0;
     }
 }
 else{
-		$sql = "SELECT id FROM teachers WHERE username='".$name."' AND passwd='".hash('sha256',$pswd)."'";
+		$sql = "SELECT id FROM teachers WHERE email='".$name."' AND passwd='".$pswd."'";
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
 		session_start();
 		while($row = $result->fetch_assoc()) {
-        $_SESSION["loggedin"] = $row["id"];
+        $_SESSION["belepve"] = $row["id"];
 		$_SESSION["level"]= 1;
     }
 }
