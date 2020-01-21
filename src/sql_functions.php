7257fedbbff6ceeda197ec51c719bf232fa511c1
andrasinorbert92 <andrasinorbert92@gmail.com>
2020-01-21 01:04:33 +0100

    ELkészítettem néhány függvényt az adatbázis eléréshez: theme_list(columns): az adott oszlopokat lekérheted a homework_theme táblából class_list_id_name: listázza az összes osztály id-jét, nevét get_userID_By_email: userID-t kérheted le email alapján role_user: a user_Role id-ját adja vissza classes_filter($classname): az osztályokat szűrheted osztáynév alapján insert_parent($email, $name, $user_id): szülőt vehetsz fel egy adott userhez id alapján
    get_table(oszlopok, tábla): bármely táblából lekérheted az oszlopokat amiket felsorolsz egy stringben
    get_table_filtered: paraméterben megadhatod a where feltételt
    get_table_filtered_ordered: a rendezési szempontot is megadhatod

--- src/sql_functions.php
+++ src/sql_functions.php
@@ -1,11 +1,59 @@
 <?php
+
 /*
-házifeltöltés
-tanár jegyet ad
-házilista
-theme lista
-class_lista
+function upload_homework(){
+	$db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
+    $db->beginTransaction();
+	
+}*/
+function get_table_filtered_ordered($columns,$table, $where, $orderby){
+	$db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
+    $db->beginTransaction();
+	
+	$select="SELECT '{$columns}' FROM '{$table}' WHERE '{$where}' ORDER BY '{$orderby}'";
+    $q_select = $db->query($select) or die("failed!");
+    $q_select->setFetchMode(PDO::FETCH_BOTH);
+    return $q_select->fetch();
+}
+function get_table_filtered($columns,$table, $where){
+	$db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
+    $db->beginTransaction();
+	
+	$select="SELECT '{$columns}' FROM '{$table}' WHERE '{$where}'";
+    $q_select = $db->query($select) or die("failed!");
+    $q_select->setFetchMode(PDO::FETCH_BOTH);
+    return $q_select->fetch();
+}
+function get_table($columns,$table){
+	$db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
+    $db->beginTransaction();
+	
+	$select="SELECT '{$columns}' FROM '{$table}'";
+    $q_select = $db->query($select) or die("failed!");
+    $q_select->setFetchMode(PDO::FETCH_BOTH);
+    return $q_select->fetch();
+}
+/*
+$columns: "id,theme..." alakú, a megjelenítendő oszlopok nevét várja
 */
+function theme_list($columns){
+	$db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
+    $db->beginTransaction();
+	
+	$select="SELECT '{$columns}' FROM homework_theme";
+    $q_select = $db->query($select) or die("failed!");
+    $q_select->setFetchMode(PDO::FETCH_BOTH);
+    return $q_select->fetch();
+}
+function class_list_id_name(){
+	$db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
+    $db->beginTransaction();
+	
+	$select_classID="SELECT * FROM classes";
+    $q_select = $db->query($select_classID) or die("failed!");
+    $q_select->setFetchMode(PDO::FETCH_BOTH);
+    return $q_select->fetch();
+}
 
 /*
     1: OK
@@ -32,74 +80,100 @@ function valid_username($email, $passwd){
         return 2;
     }
 }
+
+function get_userID_By_email($email){
+	$db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
+    $db->beginTransaction();
+	
+	$sql_get_user_id="SELECT user_id FROM users WHERE email like '{$email}'";
+    $q_userID = $db->query($sql_get_user_id) or die("failed!");
+    $q_userID->setFetchMode(PDO::FETCH_BOTH);
+    return $q_userID->fetch();
+}
+/*
+	a user role-hoz tartozó id-t adja vissza
+*/
+function role_user(){
+	
+	
+	$select_roleID="SELECT role_id FROM roles WHERE role like user";
+    $q_role = $db->query($select_roleID) or die("failed!");
+    $q_role->setFetchMode(PDO::FETCH_BOTH);
+    return $q_role->fetch();
+}
+	
+/*
+	class_name alapján visszaadja a class_id-t
+*/
+function classes_filter($classname){
+	$db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
+    $db->beginTransaction();
+	
+	$select_classID="SELECT class_id FROM classes WHERE classname like '{$classname}'";
+    $q_select = $db->query($select_classID) or die("failed!");
+    $q_select->setFetchMode(PDO::FETCH_BOTH);
+    return $q_select->fetch();
+}
 /*
     a user adatai triviálisak
     a parents az pedig tömböt vár rendezetten a szülők nevével, emailjével
+
+    csekkolni hogy hogy az email létezik e
 */
 function registration($reg_date, $email, $firstname, $lastname, $passwd, $parents_names, $parents_emails, $classname){
     $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
     $db->beginTransaction();
+	
+	//létezik-e az email:
+	$email_check_sql="SELECT COUNT(*) FROM users WHERE email like '{$email}'";
+	if($q_check=$db->query($email_check_sql)){
+		if($q_check->fetchColumn()==0){
 
-    $select_classID="SELECT class_id FROM classes WHERE classname like '{$classname}'";
-    $q_select = $db->query($select_classID) or die("failed!");
-    $q_select->setFetchMode(PDO::FETCH_BOTH);
-    $row_class=$q_select->fetch();
-    if($q_select->execute())
-        if(DEBUGMODE) echo "registration-class: OK";
-    else
-        if(DEBUGMODE) echo "registration-class: WARNING :(";
+			$row_class=classes_filter($classname);
+			$row_role=role_user();
+			
+			$sql_users= "INSERT INTO users (
+				reg_date, email, firstname, lastname, passwd, role_id, class_id)
+			VALUES(
+				:reg_date, :email, :firstname, :lastname, :passwd, :role_id, :class_id)";
+			$q=$db->prepare($sql_users);
+			$q->bindparam(':reg_date', $reg_date);
+			$q->bindparam(':email', $email);
+			$q->bindparam(':firstname', $firstname);
+			$q->bindparam(':lastname', $lastname);
+			$q->bindparam(':passwd', md5($passwd))
+			$q->bindparam(':role_id', $row_role['role_id']);
+			$q->bindparam(':class_id', $row_class['class_id']);
 
-    $select_roleID="SELECT role_id FROM roles WHERE role like user";
-    $q_role = $db->query($select_roleID) or die("failed!");
-    $q_role->setFetchMode(PDO::FETCH_BOTH);
-    $row_role=$q_role->fetch();
-    if($q_role->execute())
-        if(DEBUGMODE) echo "registration-role: OK";
-    else
-        if(DEBUGMODE) echo "registration-role: WARNING :(";
-    
-    $sql_users= "INSERT INTO users (
-        reg_date, email, firstname, lastname, passwd, role_id, class_id)
-    VALUES(
-        :reg_date, :email, :firstname, :lastname, :passwd, :role_id, :class_id)";
-    $q=$db->prepare($sql_users);
-    $q->bindparam(':reg_date', $reg_date);
-    $q->bindparam(':email', $email);
-    $q->bindparam(':firstname', $firstname);
-    $q->bindparam(':lastname', $lastname);
-    $q->bindparam(':passwd', md5($passwd))
-    $q->bindparam(':role_id', $row_role['role_id']);
-    $q->bindparam(':class_id', $row_class['class_id']);
+			if($q->execute())
+				if(DEBUGMODE) echo "registration-userInsert: OK";
+			else
+				if(DEBUGMODE) echo "registration-userInsert: WARNING :(";
 
-    if($q->execute())
-        if(DEBUGMODE) echo "registration-userInsert: OK";
-    else
-        if(DEBUGMODE) echo "registration-userInsert: WARNING :(";
-
-    $sql_get_user_id="SELECT user_id FROM users WHERE email like '{$email}'";
-    $q_userID = $db->query($sql_get_user_id) or die("failed!");
-    $q_userID->setFetchMode(PDO::FETCH_BOTH);
-    $row_userid=$q_userID->fetch();
-    if($q_userID->execute())
-        if(DEBUGMODE) echo "registration-userid: OK";
-    else
-        if(DEBUGMODE) echo "registration-userid: WARNING :(";
+			$row_userid=get_userID_By_email($email);
     
     $count_parents=count($parents_emails);
     for($i=0; $i<$count_parents; $i++){
-        $sql_parents="INSERT INTO parents (
+        insert_parent($parents_emails[$i], $parents_names[$i], $row_userid['user_id']);
+    }
+}
+
+function insert_parent($email, $name, $user_id){
+	$db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE.';port='.DB_PORT, DB_USERNAME, DB_PASSWORD);
+    $db->beginTransaction();
+	
+	$sql_parents="INSERT INTO parents (
             email, name, user_id)
         VALUES(
             :email, :name, :user_id)";
-        $q_parents=$db->prepare($sql_parents);
-        $q_parents->bindparam(':email', $parents_emails[$i]);
-        $q_parents->bindparam(':name', $parents_names[$i]);
-        $q_parents->bindparam(':user_id', $row_userid['user_id']);
-        if($q_parents->execute())
-            if(DEBUGMODE) echo "registration-parents: OK";
-        else
-            if(DEBUGMODE) echo "registration-parents: WARNING :(";
-    }
+	$q_parents=$db->prepare($sql_parents);
+	$q_parents->bindparam(':email', $email);
+	$q_parents->bindparam(':name', $name);
+	$q_parents->bindparam(':user_id', $user_id);
+	if($q_parents->execute())
+		if(DEBUGMODE) echo "registration-parents: OK";
+	else
+		if(DEBUGMODE) echo "registration-parents: WARNING :(";
 }
 
 function dd($var){ //dumpdie
